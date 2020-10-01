c7f345fa1f628d874e88433ca475cd581e850693
asavage7 <62650275+asavage7@users.noreply.github.com>
2020-10-01 16:34:58 -0400

    added firebase

---
+++ assets/js/firebase.js
@@ -0,0 +1,15 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDm4nrcpH2wgPHkBcwsGVG8w7Qe44KdZKY",
+    authDomain: "pcs-site-90ded.firebaseapp.com",
+    databaseURL: "https://pcs-site-90ded.firebaseio.com",
+    projectId: "pcs-site-90ded",
+    storageBucket: "pcs-site-90ded.appspot.com",
+    messagingSenderId: "875942728403",
+    appId: "1:875942728403:web:0a88b107af133b3062ba26",
+    measurementId: "G-4NMNM2BRTW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
